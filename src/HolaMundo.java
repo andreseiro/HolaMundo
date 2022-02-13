6ff4f7d7ffb6a8ba6e7ad84b199507fd1e096f79
Andreseiro <andreseiro@MacBook-Air-de-Andres.local>
2022-02-13 18:14:49 -0500

    actualizacion final

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,39 +1,12 @@
-//Programa de prueba
-
+/**
+ * @author andreseiro
+ * @Fecha: Febrero de 2022
+ *
+ */
 
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo\n");
-		
-		//Tipos de datos enteros
-		byte edad = 43;
-		short year = 1990;
-		int id_user = 1001;
-		long id_twitter = 1233345345345345343L;
-		
-		System.out.println("La edad es: " + edad);
-		System.out.println("El año es: " + year);
-		System.out.println("El usuario es: " + id_user);
-		System.out.println("El id de twitter es: " + id_twitter);
-		System.out.println();
-		
-		//Tipos de dato decimal. punto flotante
-		float diametro = 34.2F;
-		double precio = 23412.243432433;
-		System.out.println("El diametro es: " + diametro);
-		System.out.println("El precio es: " + precio);
-		System.out.println();
-		
-		
-		//tipos de datos texto
-		char genero = 'M';
-		
-		//tipos de datos logicos
-		boolean isVisible = true;
-		boolean funciona = false;
-		
-		
-		
+		System.out.println("Hola Mundo");				
 	}
 }
